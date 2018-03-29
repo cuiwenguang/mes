4de6d5b39bcf79637b4946f4e40091735f7f09fe
cwg <cuiwenguang@gsoft.cc>
2018-03-29 18:40:50 +0800

    static

--- meat/models.py
+++ meat/models.py
@@ -15,12 +15,12 @@ class CollectInfo(models.Model):
     collect_no = models.CharField(max_length=50) # 交易流水号
     creat_at = models.DateTimeField()  # 收购时间
     batch_number = models.CharField(max_length=20) # 收购批次
-    customer = models.ForeignKey(Customer) # 客户账号
+    customer = models.ForeignKey(Customer, on_delete=None) # 客户账号
     collect_weight = models.FloatField(default=0)  # 称重重量
     collect_number = models.IntegerField(default=0)  # 收购数量
     standard = models.CharField(max_length=20) # 产品规格
     c_type = models.CharField(max_length=20) # 品种
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(User, on_delete=None)
 
 
 class ButcheringInfo(models.Model):
@@ -28,12 +28,12 @@ class ButcheringInfo(models.Model):
     collect_no = models.CharField(max_length=50)  # 交易流水号
     creat_at = models.DateTimeField()  # 收购时间
     batch_number = models.CharField(max_length=20)  # 收购批次
-    customer = models.ForeignKey(Customer)  # 客户账号
+    customer = models.ForeignKey(Customer, on_delete=None)  # 客户账号
     collect_weight = models.FloatField(default=0)  # 称重数量
     collect_number = models.IntegerField(default=0)  # 收购数量
     standard = models.CharField(max_length=20)  # 产品规格
     c_type = models.CharField(max_length=20)  # 品种
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(User, on_delete=None)
 
 
 class ButcheredInfo(models.Model):
@@ -41,13 +41,13 @@ class ButcheredInfo(models.Model):
     collect_no = models.CharField(max_length=50)  # 交易流水号
     creat_at = models.DateTimeField()  # 收购时间
     batch_number = models.CharField(max_length=20)  # 收购批次
-    customer = models.ForeignKey(Customer)  # 客户账号
+    customer = models.ForeignKey(Customer, on_delete=None)  # 客户账号
     collect_weight = models.FloatField(default=0)  # 称重数量
     collect_number = models.IntegerField(default=0)  # 收购数量
     standard = models.CharField(max_length=20)  # 产品规格
     c_type = models.CharField(max_length=20)  # 品种
     level = models.CharField(max_length=20) # 等级
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(User, on_delete=None)
     ps_weight = models.FloatField(default=0)  #排酸重量
 
 
--- mes/settings.py
+++ mes/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '4mhsyi9z!^w-dygvq756wy0&)fve0)+yck#w4n9by8@480&gkc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'meat',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'mes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,5 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
 TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
