60c127477038deb7def81d6860faa9379d3af768
cui <7211334@qq.com>
2018-03-31 01:41:56 +0800

    sg ui

--- meat/models.py
+++ meat/models.py
@@ -2,30 +2,28 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-
+'''
 class Customer(models.Model):
-    '''收购对象：农牧民账户信息'''
     cust_name = models.CharField(max_length=20)
     id_card = models.CharField(max_length=20)
     address = models.CharField(max_length=255)
     mobile = models.CharField(max_length=20)
 
+
 class CollectInfo(models.Model):
-    collect_no = models.CharField(max_length=50) # 交易流水号
+    collect_no = models.CharField(max_length=50)  # 交易流水号
     creat_at = models.DateTimeField()  # 收购时间
-    batch_number = models.CharField(max_length=20) # 收购批次
-    customer = models.ForeignKey(Customer, on_delete=None) # 客户账号
+    batch_number = models.CharField(max_length=20)  # 收购批次
+    customer = models.ForeignKey(Customer, on_delete=None)  # 客户账号
     collect_weight = models.FloatField(default=0)  # 收购称重重量
-    wait_weight = models.FloatField(default=0)  # 待宰前称重数量
-    butcher_weight = models.FloatField(default=0)  # 屠宰后称重数量
+    wait_weight = models.FloatField(default=0)  # 待宰前称重重量
+    butcher_weight = models.FloatField(default=0)  # 屠宰后称重量
     collect_number = models.IntegerField(default=0)  # 收购数量
-    standard = models.CharField(max_length=20) # 产品规格
-    c_type = models.CharField(max_length=20) # 品种
+    standard = models.CharField(max_length=20)  # 产品规格
+    c_type = models.CharField(max_length=20)  # 品种
     user = models.ForeignKey(User, on_delete=None)
 
 
-
-'''
 class CollectInfo(models.Model):
     collect_no = models.CharField(max_length=50) # 交易流水号
     creat_at = models.DateTimeField()  # 收购时间
