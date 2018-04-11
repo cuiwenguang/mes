3d730777b37f1a0a61a608976a312df09faf9199
cuiwenguang <cui_wenguang@163.com>
2018-04-12 01:16:08 +0800

    活体收购界面， 使用bootstraptable取代jquerytable

--- static/js/page/sg_edit.js
+++ static/js/page/sg_edit.js
@@ -1,13 +1,30 @@
-var page = {};
+var totalWeight = 0;
 var count = 0;
+var totalNumber = 0;
+
 $(function () {
-   page.table = $("#table1").bootstrapTable({
-       data: [
-            count++,
-            $("#cur_number").val(),
-            $("#mz_weight").val(),
-            $("#pz_weight").val(),
-            $("#jz_weight").val()
-       ]
-   });
+    $("#table1").bootstrapTable({});
 });
+
+$("#btnSave").click(function () {
+   $("#table1").bootstrapTable('insertRow',{
+      index: 0,
+       row:{
+          no:++count,
+         cur_number: $("#cur_number").val(),
+         mz_weight: $("#mz_weight").val(),
+         pz_weight: $("#pz_weight").val(),
+         jz_weight: $("#jz_weight").val(),
+         opt: "<buttn type='button' onclick='delRow("+count+")'>删除</buttn>"
+       }
+   });
+   totalWeight += parseFloat($("#jz_weight").val());
+   $("#lblWeight").text(totalWeight)
+   totalNumber += parseInt($("#cur_number").val());
+   $("#lblNumber").text(totalNumber);
+   $("#lblCount").text(count);
+});
+
+function delRow(index) {
+    $("#table1").bootstrapTable('remove', {field:"no", values:[index]});
+}
