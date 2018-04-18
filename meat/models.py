538aaf619d032ef43fbba5938eef0069bc929ab7
cwg <cuiwenguang@gsoft.cc>
2018-04-18 19:07:34 +0800

    完成酮体称重界面

--- meat/models.py
+++ meat/models.py
@@ -93,42 +93,19 @@ class CollectInfo(models.Model):
                 d[attr] = getattr(self, attr)
         return d
 
-'''
-class CollectInfo(models.Model):
-    collect_no = models.CharField(max_length=50) # 交易流水号
-    creat_at = models.DateTimeField()  # 收购时间
-    batch_number = models.CharField(max_length=20) # 收购批次
-    customer = models.ForeignKey(Customer, on_delete=None) # 客户账号
-    collect_weight = models.FloatField(default=0)  # 称重重量
-    collect_number = models.IntegerField(default=0)  # 收购数量
-    standard = models.CharField(max_length=20) # 产品规格
-    c_type = models.CharField(max_length=20) # 品种
-    user = models.ForeignKey(User, on_delete=None)
 
 
-class ButcheringInfo(models.Model):
-    collect_no = models.CharField(max_length=50)  # 交易流水号
-    creat_at = models.DateTimeField()  # 收购时间
-    batch_number = models.CharField(max_length=20)  # 收购批次
-    customer = models.ForeignKey(Customer, on_delete=None)  # 客户账号
-    collect_weight = models.FloatField(default=0)  # 称重数量
-    collect_number = models.IntegerField(default=0)  # 收购数量
-    standard = models.CharField(max_length=20)  # 产品规格
-    c_type = models.CharField(max_length=20)  # 品种
-    user = models.ForeignKey(User, on_delete=None)
+class RawInfo(models.Model):
+    """原料集冻库"""
+    datetime_at = models.DateTimeField(auto_now_add=True)
+    sc_no = models.CharField(max_length=50)
+    sg_no = models.CharField(max_length=50)
 
 
-class ButcheredInfo(models.Model):
-    collect_no = models.CharField(max_length=50)  # 交易流水号
-    creat_at = models.DateTimeField()  # 收购时间
-    batch_number = models.CharField(max_length=20)  # 收购批次
-    customer = models.ForeignKey(Customer, on_delete=None)  # 客户账号
-    collect_weight = models.FloatField(default=0)  # 称重数量
-    collect_number = models.IntegerField(default=0)  # 收购数量
-    standard = models.CharField(max_length=20)  # 产品规格
-    c_type = models.CharField(max_length=20)  # 品种
-    level = models.CharField(max_length=20) # 等级
-    user = models.ForeignKey(User, on_delete=None)
-    ps_weight = models.FloatField(default=0)  #排酸重量
 
-'''
+class ProductInfo(models.Model):
+    sc_no = models.CharField(max_length=50)
+    product_no = models.IntegerField()
+    product_name = models.CharField(max_length=50)
+    product_weight = models.FloatField()
+
